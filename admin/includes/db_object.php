9dabdba79b444f174bdcf30dc59ef162534692af
Patryk <patryk.r.nowak@gmail.com>
2023-01-05 22:21:14 +0000

    photo class

--- admin/includes/db_object.php
+++ admin/includes/db_object.php
@@ -1,32 +1,38 @@
 <?php
 
+
+// https://www.php.net/manual/en/language.oop5.late-static-bindings.php
+
+// https://stackoverflow.com/questions/1912902/what-exactly-are-late-static-bindings-in-php
 class Db_object
 {
-    protected static $db_table = "";
+    // TODO: READ more
+    // it is working because of the static:$db_table 
+    // protected static $db_table = "";
 
     static function find_all()
     {
         // https://www.geeksforgeeks.org/what-is-late-static-bindings-in-php/
         // https://www.php.net/manual/en/language.oop5.late-static-bindings.php
         // one way to fix bugs
-        // $this->find_this_query.....
+        // $this->find_by_query.....
         // but then you can not use static method
 
 
 
 
-        return static::find_this_query("SELECT * FROM " . static::$db_table . " ");
+        return static::find_by_query("SELECT * FROM " . static::$db_table . " ");
     }
 
     static function find_by_id($id)
     {
         global $database;
-        $the_result_array = static::find_this_query("SELECT * FROM " . static::$db_table . " WHERE id = $id LIMIT 1");
+        $the_result_array = static::find_by_query("SELECT * FROM " . static::$db_table . " WHERE id = $id LIMIT 1");
 
         // ternary version in php
         return !empty($the_result_array) ? array_shift($the_result_array) : false;
     }
-    public static function find_this_query($sql)
+    public static function find_by_query($sql)
     {
         global $database;
         $result_set = $database->query($sql);
@@ -65,5 +71,121 @@ class Db_object
         $object_properties = get_object_vars($this);
 
         return array_key_exists($attribute, $object_properties);
+    }
+
+    protected function properties()
+    {
+        // return get_object_vars($this);
+
+        $properties = array();
+
+        foreach (static::$db_table_fields as $db_field) {
+
+            if (property_exists($this, $db_field)) {
+
+                $properties[$db_field] = $this->$db_field;
+            }
+        }
+
+        return $properties;
+    }
+
+    protected function clean_properties()
+    {
+        global $database;
+
+        $clean_properties = array();
+
+        foreach ($this->properties() as $key => $value) {
+            $clean_properties[$key] = $database->escape_string($value);
+        }
+
+        return $clean_properties;
+    }
+
+
+
+    public function save()
+    {
+        return isset($this->id) ? $this->update() : $this->create();
+    }
+
+    public function create()
+    {
+        global $database;
+
+        $properties = $this->clean_properties();
+
+        // https://www.php.net/manual/en/function.implode.php
+        // https://www.w3schools.com/php/func_string_implode.asp 
+        $sql = "INSERT INTO " . static::$db_table .  "(" . implode(',', array_keys($properties)) . ")";
+        $sql .= "VALUES('" .  implode("','", array_values($properties))   . "')";
+
+        // echo $sql;
+        // users(id,username,password,firstname,lastname)VALUES('','static_user','static','first_name','last_name')
+        // after upgrade
+        // users(username,password,firstname,lastname)VALUES('static_user','static','first_name','last_name')
+
+        $query_status =  $database->query($sql);
+        if ($query_status) {
+
+            $this->id = $database->the_insert_id();
+
+            return true;
+        } else {
+            return false;
+        }
+    } // create method
+
+
+    public function update()
+    {
+        global $database;
+
+        $properties = $this->clean_properties();
+
+        $properties_pairs = array();
+
+        foreach ($properties as $key => $value) {
+            $properties_pairs[] = "{$key}='{$value}'";
+        }
+
+        $sql = "UPDATE " . static::$db_table .  " SET ";
+
+        $sql .= implode(", ", $properties_pairs);
+
+
+        // $sql .= "username= '" . $database->escape_string(($this->username)) . "', ";
+        // $sql .= "password= '" . $database->escape_string(($this->password)) . "', ";
+        // $sql .= "firstname= '" . $database->escape_string(($this->firstname))  . "', ";
+        // $sql .= "lastname= '" . $database->escape_string(($this->lastname))  . "' ";
+        $sql .= " WHERE id= " .   $database->escape_string(($this->id));
+
+        // echo $sql;
+        // UPDATE users SET username='Whatever 2', password='static', firstname='first_name', lastname='last_name' WHERE id= 14
+        $query_status =  $database->query($sql);
+
+        return (mysqli_affected_rows($database->connection) == 1) ? true : false;
+    }
+
+
+
+    public function delete()
+    {
+        global $database;
+
+        $sql = "DELETE FROM " . static::$db_table .  " ";
+        $sql .= "WHERE id= " .  $database->escape_string(($this->id)) . " ";
+        $sql .= "LIMIT 1";
+
+        $query_status =  $database->query($sql);
+        // if ($query_status) {
+
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+
+        return (mysqli_affected_rows($database->connection) == 1) ? true : false;
     }
 }
--- admin/includes/user.php
+++ admin/includes/user.php
@@ -13,12 +13,6 @@ class User extends Db_object
     public $firstname;
     public $lastname;
 
-
-
-
-
-
-
     public static function verify_user($username, $password)
     {
         global $database;
@@ -33,7 +27,7 @@ class User extends Db_object
         // ???
         $sql .= "LIMIT 1";
 
-        $the_result_array = self::find_this_query($sql);
+        $the_result_array = self::find_by_query($sql);
 
         // ternary version in php
         // return !empty($the_result_array) ? array_shift($the_result_array) : false;
@@ -46,118 +40,10 @@ class User extends Db_object
         }
     }
 
-    protected function properties()
-    {
-        // return get_object_vars($this);
-
-        $properties = array();
-
-        foreach (self::$db_table_fields as $db_field) {
-
-            if (property_exists($this, $db_field)) {
-
-                $properties[$db_field] = $this->$db_field;
-            }
-        }
-
-        return $properties;
-    }
-
-    protected function clean_properties()
-    {
-        global $database;
-
-        $clean_properties = array();
-
-        foreach ($this->properties() as $key => $value) {
-            $clean_properties[$key] = $database->escape_string($value);
-        }
-
-        return $clean_properties;
-    }
-
-
-    public function save()
-    {
-        return isset($this->id) ? $this->update() : $this->create();
-    }
-
-    public function create()
-    {
-        global $database;
-
-        $properties = $this->clean_properties();
-
-        // https://www.php.net/manual/en/function.implode.php
-        // https://www.w3schools.com/php/func_string_implode.asp 
-        $sql = "INSERT INTO " . self::$db_table .  "(" . implode(',', array_keys($properties)) . ")";
-        $sql .= "VALUES('" .  implode("','", array_values($properties))   . "')";
-
-        // echo $sql;
-        // users(id,username,password,firstname,lastname)VALUES('','static_user','static','first_name','last_name')
-        // after upgrade
-        // users(username,password,firstname,lastname)VALUES('static_user','static','first_name','last_name')
-
-        $query_status =  $database->query($sql);
-        if ($query_status) {
-
-            $this->id = $database->the_insert_id();
-
-            return true;
-        } else {
-            return false;
-        }
-    } // create method
-
-
-    public function update()
-    {
-        global $database;
-
-        $properties = $this->clean_properties();
-
-        $properties_pairs = array();
-
-        foreach ($properties as $key => $value) {
-            $properties_pairs[] = "{$key}='{$value}'";
-        }
-
-        $sql = "UPDATE " . self::$db_table .  " SET ";
-
-        $sql .= implode(", ", $properties_pairs);
-
-
-        // $sql .= "username= '" . $database->escape_string(($this->username)) . "', ";
-        // $sql .= "password= '" . $database->escape_string(($this->password)) . "', ";
-        // $sql .= "firstname= '" . $database->escape_string(($this->firstname))  . "', ";
-        // $sql .= "lastname= '" . $database->escape_string(($this->lastname))  . "' ";
-        $sql .= " WHERE id= " .   $database->escape_string(($this->id));
-
-        // echo $sql;
-        // UPDATE users SET username='Whatever 2', password='static', firstname='first_name', lastname='last_name' WHERE id= 14
-        $query_status =  $database->query($sql);
-
-        return (mysqli_affected_rows($database->connection) == 1) ? true : false;
-    }
 
 
 
-    public function delete()
-    {
-        global $database;
 
-        $sql = "DELETE FROM " . self::$db_table .  " ";
-        $sql .= "WHERE id= " .  $database->escape_string(($this->id)) . " ";
-        $sql .= "LIMIT 1";
 
-        $query_status =  $database->query($sql);
-        // if ($query_status) {
-
-        //     return true;
-        // } else {
-        //     return false;
-        // }
-
-        return (mysqli_affected_rows($database->connection) == 1) ? true : false;
-    }
+    
 } // end of the user class
